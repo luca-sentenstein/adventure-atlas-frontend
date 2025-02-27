58b1ebcf2e2310d80a1ccece65de2707aeaf8e5b
Leon Baisch <baischl.tin24@student.dhbw-heidenheim.de>
2025-02-27 22:19:41 +0100

    Created simple text input field which allows for autocompletion with Google Maps API and fetches data from the API for a selected location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { PlaceAutocompleteComponent } from './components/place-autocomplete/place-autocomplete.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { PlaceSearchResult } from './interfaces/place-search-result.interface';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, CommonModule, PlaceAutocompleteComponent, MatToolbarModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
     title = 'adventure-atlas';
+    fromValue: PlaceSearchResult | undefined;
 }

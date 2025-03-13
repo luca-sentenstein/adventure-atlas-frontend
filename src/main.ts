95a8b97512926ce7c15c5d235afa548fbbab2e91
Davidik <malanind.tin24@student.dhbw-heidenheim.de>
2025-03-13 11:09:42 +0100

    1.Implementierung

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,26 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
     .catch((err) => console.error(err));
+
+document.addEventListener('DOMContentLoaded', () => {
+    const interactiveBubble = document.querySelector('.interactive') as HTMLElement | null;
+    if (!interactiveBubble) return;
+
+    let mouseX = 0, mouseY = 0, curX = 0, curY = 0;
+
+    function moveBubble() {
+        if (interactiveBubble) {
+            curX += (mouseX - curX) / 10;
+            curY += (mouseY - curY) / 10;
+            interactiveBubble.style.transform = `translate(${curX}px, ${curY}px)`;
+        }
+        requestAnimationFrame(moveBubble);
+    }
+
+    window.addEventListener('mousemove', (event) => {
+        mouseX = event.clientX;
+        mouseY = event.clientY;
+    });
+
+    moveBubble();
+});
